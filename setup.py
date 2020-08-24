7a4fc7315e73443fd86b1810de8603450433527c
mithun <mithunpaul08@gmail.com>
2020-08-23 18:18:05 -0700

    previous commit did work . trying with setup just in case it work also. its more elegant

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='Distutils',
+setup(name='MyPythonProject',
       version='1.0',
-      description='Python Distribution Utilities',
-      author='Greg Ward',
-      author_email='gward@python.net',
+      description='What it does',
+      author='mithun',
+      author_email='mithunpaul@email.arizona.edu',
       url='https://www.python.org/sigs/distutils-sig/',
-      packages=['distutils', 'distutils.command'],
+      packages=find_packages(),
      )
